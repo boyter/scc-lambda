cc6c87437ce7094ecb4d05bc10705107741ae32d
Ben Boyter <ben@boyter.org>
2019-06-27 08:10:56 +1000

    Add maxage header

--- lambda.py
+++ lambda.py
@@ -74,7 +74,8 @@ def lambda_handler(event, context):
         "statusDescription": "200 OK",
         "isBase64Encoded": False,
         "headers": {
-            "Content-Type": "image/svg+xml;charset=utf-8"
+            "Content-Type": "image/svg+xml;charset=utf-8",
+            "Cache-Control": "max-age=86400"
         },
         "body": '''<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100" height="20"><linearGradient id="b" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><clipPath id="a"><rect width="100" height="20" rx="3" fill="#fff"/></clipPath><g clip-path="url(#a)"><path fill="#555" d="M0 0h69v20H0z"/><path fill="#4c1" d="M69 0h31v20H69z"/><path fill="url(#b)" d="M0 0h100v20H0z"/></g><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="110"> <text x="355" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="590">''' + title + '''</text><text x="355" y="140" transform="scale(.1)" textLength="590">''' + title +'''</text><text x="835" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="''' + text_length + '''">''' + s + '''</text><text x="835" y="140" transform="scale(.1)" textLength="''' + text_length + '''">''' + s + '''</text></g> </svg>'''
     }
